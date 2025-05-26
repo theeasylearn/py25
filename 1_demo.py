797119f9c097bfb644f3e7bf1fcd626e6dc003ee
Dpatelit <dpatel99.it@gmail.com>
2025-05-26 08:52:34 +0530

    one

--- 1_demo.py
+++ 1_demo.py
@@ -1,7 +1,7 @@
 print("hello ")
 a ="The easylearn academy"
-print(a)
-print("We are learning python at",a)
+# print(a)
+# print("We are learning python at",a)
 # now i change value of variable a
 a=10
 print(a)
